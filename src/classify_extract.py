c5553ee218d2494779e8b6b9baab5d43c4427340
daniele <d.malitesta@gmail.com>
2020-03-04 09:57:50 +0100

    Added cnn codes.

--- src/classify_extract.py
+++ src/classify_extract.py
@@ -6,22 +6,26 @@ import torchvision.models as models
 import pandas as pd
 import sys
 
-path_images, path_output_classes, path_classes = read_config(config_file='./config/configs.ini',
-                                                             sections_fields=[('PATHS', 'InputAmazonMenImages'),
-                                                                              ('PATHS', 'OutputAmazonMenClasses'),
-                                                                              ('PATHS', 'ImagenetClasses')])
-data = CustomDataset(root_dir=path_images)
-model = Model(model=models.resnet50(pretrained=True))
-img_classes = read_imagenet_classes_txt(path_classes)
+def classify_and_extract():
+    path_images, path_output_classes, path_classes = read_config(config_file='./config/configs.ini',
+                                                                 sections_fields=[('PATHS', 'InputAmazonMenImages'),
+                                                                                  ('PATHS', 'OutputAmazonMenClasses'),
+                                                                                  ('PATHS', 'ImagenetClasses')])
+    data = CustomDataset(root_dir=path_images)
+    model = Model(model=models.resnet50(pretrained=True))
+    img_classes = read_imagenet_classes_txt(path_classes)
 
-df = pd.DataFrame([], columns={'ImageID', 'ClassStr', 'ClassNum'})
+    df = pd.DataFrame([], columns={'ImageID', 'ClassStr', 'ClassNum'})
 
-for i, d in enumerate(data):
-    out_class = model.classification(list_classes=img_classes, sample=d)
-    feature = model.feature_extraction(sample=d)
-    df = df.append(out_class, ignore_index=True)
-    sys.stdout.write('\r%d/%d samples completed' % (i + 1, data.num_samples))
-    sys.stdout.flush()
+    for i, d in enumerate(data):
+        out_class = model.classification(list_classes=img_classes, sample=d)
+        feature = model.feature_extraction(sample=d)
+        df = df.append(out_class, ignore_index=True)
+        sys.stdout.write('\r%d/%d samples completed' % (i + 1, data.num_samples))
+        sys.stdout.flush()
 
-df = df.sort_values(by=['ImageID'])
-write_csv(df, path_output_classes)
+    df = df.sort_values(by=['ImageID'])
+    write_csv(df, path_output_classes)
+
+if __name__ == '__main__':
+    classify_and_extract()
