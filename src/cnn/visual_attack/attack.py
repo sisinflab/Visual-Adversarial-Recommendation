58302a7f17ceae0da49e314b11f3b803736533d4
daniele <d.malitesta@gmail.com>
2020-03-09 15:51:13 +0100

    Visual attacks added.

--- src/cnn/visual_attack/attack.py
+++ src/cnn/visual_attack/attack.py
@@ -31,6 +31,7 @@ class VisualAttack:
         self.params["y_target"] = self.y_target
 
     def must_attack(self, filename):
+        print(filename)
         print(self.df_classes.loc[self.df_classes["ImageID"] == filename])
         if int(self.df_classes.loc[self.df_classes["ImageID"] == filename]["ClassNum"]) == self.origin_class:
             return True
