6ec3e6586681c0136e328143cce67c5e143b13c9
daniele <d.malitesta@gmail.com>
2020-03-10 17:51:55 +0100

    Choose gpu to run the script.

--- src/cnn/visual_attack/attack.py
+++ src/cnn/visual_attack/attack.py
@@ -52,9 +52,6 @@ class VisualAttack:
         if self.attack_type in ['cw', 'jsma']:
             self.x_op = tf.reshape(self.x_op, shape=(1, 3, image.shape[1], image.shape[2]))
 
-        if self.attack_type == 'jsma':
-            self.params["y_target"] = tf.Variable(self.params["y_target"], dtype=tf.uint8)
-
         self.adv_x_op = self.attack_op.generate(self.x_op, **self.params)
         adv_img = self.sess.run(self.adv_x_op, feed_dict={self.x_op: image[None, ...]})
         adv_img_out = transforms.ToTensor()(adv_img[0])
