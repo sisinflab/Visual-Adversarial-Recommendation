aa2aec71bee3ddcc8218e416178e0fced7ed0c7a
MerraEng <merrafelice@gmail.com>
2020-03-12 17:18:14 +0100

    update

--- src/cnn/visual_attack/attack.py
+++ src/cnn/visual_attack/attack.py
@@ -56,27 +56,31 @@ class VisualAttack:
             # Obtain Image Parameters
             image = image.cpu().numpy()
             img_row, img_col, nchannel = image.shape[1], image.shape[2], image.shape[0]
-            nb_classes = self.num_classes
+            # nb_classes = self.num_classes
+            nb_classes = 1
 
             adv_inputs = np.array(
-                [[instance] * nb_classes for
+                [[instance] for
                  instance in [image]], dtype=np.float32)
 
-            one_hot = np.zeros((nb_classes, nb_classes))
-            one_hot[np.arange(nb_classes), np.arange(nb_classes)] = 1
+            one_hot = np.zeros((1, 1000))
+            # one_hot[np.arange(nb_classes), np.arange(nb_classes)] = 1
+            one_hot[0,770 ] = 1
 
             adv_inputs = adv_inputs.reshape(
                 (nb_classes, nchannel, img_row, img_col))
             adv_ys = np.array([one_hot],
-                              dtype=np.float32).reshape((nb_classes, nb_classes))
+                              dtype=np.float32).reshape((nb_classes, 1000))
 
             # adv_inputs = adv_inputs.reshape((nb_classes, nchannel, img_row, img_col))
             self.x_op = tf.reshape(self.x_op, shape=(nb_classes, 3, image.shape[1], image.shape[2]))
             self.params["y_target"] = adv_ys
             self.adv_x_op = self.attack_op.generate(self.x_op, **self.params)
 
-            adv_img = self.sess.run(self.adv_x_op, feed_dict={self.x_op: adv_inputs})[self.target_class]
-            return adv_img
+            adv_img = self.sess.run(self.adv_x_op, feed_dict={self.x_op: adv_inputs})
+            adv_img_out = transforms.ToTensor()(adv_img[0])
+            adv_img_out = adv_img_out.permute(1, 2, 0)
+            return adv_img_out
 
             # self.x_op = tf.reshape(self.x_op, shape=(1, 3, image.shape[1], image.shape[2]))
             # self.x_op = tf.reshape(self.x_op, shape=tf.cast(tf.expand_dims(image, 1), tf.int32))
