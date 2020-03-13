b79cb64cf40ed7dcf549803a6340130d350ac4ed
daniele <d.malitesta@gmail.com>
2020-03-13 16:29:27 +0100

    Added normalization to classify_extract.py

--- src/classify_extract.py
+++ src/classify_extract.py
@@ -18,7 +18,8 @@ def classify_and_extract():
     data = CustomDataset(root_dir=path_images,
                          transform=transforms.Compose([
                              transforms.ToTensor(),
-                             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                  std=[0.229, 0.224, 0.225])
                          ]))
     model = Model(model=models.resnet50(pretrained=True))
     model.set_out_layer(drop_layers=1)
