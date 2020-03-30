1c272a1d049f28d3f35fea3c6b997166b311d296
MerraEng <merrafelice@gmail.com>
2020-03-30 19:05:33 +0200

    results analyzer for special cases

--- src/classify_extract_attack.py
+++ src/classify_extract_attack.py
@@ -305,6 +305,9 @@ def classify_and_extract_attack():
             if (i + 1) % 1 == 0:
                 print('%d/%d samples completed' % (i + 1, data.num_samples))
 
+            if i == 5000:
+                break
+
     # Save all extracted features (attacked and non-attacked ones)
     save_np(npy=features, filename=path_output_features_attack)
 
