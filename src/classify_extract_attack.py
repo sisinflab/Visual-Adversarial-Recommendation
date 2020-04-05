33681949d9bb79bda8b7321312657e9d80b58442
MerraEng <merrafelice@gmail.com>
2020-04-05 11:18:30 +0200

    zoo

--- src/classify_extract_attack.py
+++ src/classify_extract_attack.py
@@ -314,10 +314,9 @@ def classify_and_extract_attack():
                          # reshape=True,
                          # scale=4,
                          transform=transforms.Compose([
-                             transforms.ToTensor()
-                             # ,
-                             # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                             #                      std=[0.229, 0.224, 0.225])
+                             transforms.ToTensor(),
+                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                                  std=[0.229, 0.224, 0.225])
                          ])
                          )
     model = Model(model=models.resnet50(pretrained=True))
