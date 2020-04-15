7c855f2751b91ed939d07cac5bd992f61ab47f39
daniele <d.malitesta@gmail.com>
2020-04-15 10:05:02 +0200

    added reshape for spsa and zoo.

--- src/cnn/visual_attack/zoo_l2.py
+++ src/cnn/visual_attack/zoo_l2.py
@@ -142,10 +142,14 @@ def coordinate_Newton_ADAM(losses, indice, grad, hess, batch_size, mt_arr, vt_ar
 class ZOOL2:
     def __init__(self, sess, model, height=224, width=224, batch_size=128, num_channels=3, num_labels=1000,
                  confidence=CONFIDENCE, targeted=True, learning_rate=LEARNING_RATE,
-                 binary_search_steps=BINARY_SEARCH_STEPS, max_iterations=10000, print_every=100,
+                 binary_search_steps=BINARY_SEARCH_STEPS, max_iterations=2000, print_every=100,
                  early_stop_iters=0, abort_early=ABORT_EARLY, initial_const=10.0, use_log=False, use_tanh=True,
                  use_resize=True, adam_beta1=0.9, adam_beta2=0.999, reset_adam_after_found=True, solver="adam",
                  save_ckpts="", load_checkpoint="", start_iter=0, init_size=32, use_importance=True):
+
+        ##############################################
+        # max_iterations set to 2000 instead of 10000
+        ##############################################
         """
         The L_2 optimized attack.
 
