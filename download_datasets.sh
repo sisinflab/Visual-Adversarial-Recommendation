a2852b3a889e119d89880d8ce526d90d4a0bbdbc
daniele <d.malitesta@gmail.com>
2020-05-15 16:56:22 +0200

    submission id: 1883

--- download_datasets.sh
+++ download_datasets.sh
@@ -3,3 +3,11 @@
 wget https://srv-file9.gofile.io/download/QmRlVq/data.zip
 unzip data.zip
 rm data.zip
+
+mkdir -p ./rec_results/amazon_men/
+mkdir -p ./rec_results/amazon_women/
+mkdir -p ./rec_results/tradesy/
+
+mkdir -p ./rec_model_weights/amazon_men/
+mkdir -p ./rec_model_weights/amazon_women/
+mkdir -p ./rec_model_weights/tradesy/
