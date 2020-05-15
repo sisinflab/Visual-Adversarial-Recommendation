b2bee62c28106bfefd408d771b2166f66e603cbd
daniele <d.malitesta@gmail.com>
2020-05-15 17:08:08 +0200

    submission id: 1883

--- download_datasets.sh
+++ download_datasets.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+# download datasets
 wget https://srv-file9.gofile.io/download/QmRlVq/data.zip
 unzip data.zip
 rm data.zip
 
+# create directories recommendation
 mkdir -p ./rec_results/amazon_men/
 mkdir -p ./rec_results/amazon_women/
 mkdir -p ./rec_results/tradesy/
@@ -12,3 +14,10 @@ mkdir -p ./rec_model_weights/amazon_men/
 mkdir -p ./rec_model_weights/amazon_women/
 mkdir -p ./rec_model_weights/tradesy/
 
+mkdir -p ./chr/amazon_men/
+mkdir -p ./chr/amazon_women/
+mkdir -p ./chr/tradesy/
+
+mkdir -p ./ncdcg/amazon_men/
+mkdir -p ./ncdcg/amazon_women/
+mkdir -p ./ncdcg/tradesy/
